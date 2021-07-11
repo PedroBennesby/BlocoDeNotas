f74095ce0c09d677bc83358e16e197478377b7bf
Pedro Bennesby <pedrobennesby@gmail.com>
2021-07-11 12:46:07 -0300

    feat: adicionado delete de notas

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,11 +1,20 @@
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { Component } from "react";
 import "./estilo.css";
 class CardNota extends Component {
+
+  apagar() {
+    const indice = this.props.indice
+    this.props.apagarNota(indice)
+  }
+
   render() {
     return (
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+          <FontAwesomeIcon icon={faTrash} onClick={this.apagar.bind(this)}/>
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
       </section>
